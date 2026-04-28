6c44481b2fe4336d0d3a546d4170786e069bb64a
aquatictest <aquatictest@proton.me>
2026-04-28 17:19:49 +0900

    3장 종료

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+mod extract;
+
 use axum::{Router, extract::Path, routing::get};
+
+use crate::extract::RequestID;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new()
         .route("/", get(hello))
-        .route("/posts/{id}", get(get_post));
+        .route("/posts/{id}", get(get_post))
+        .route("/whoami", get(whoami));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
     println!(
@@ -22,4 +27,8 @@ async fn hello() -> &'static str {
 
 async fn get_post(Path(id): Path<i64>) -> String {
     format!("post id = {id}")
+}
+
+async fn whoami(request_id: RequestID) -> String {
+    format!("your request id is {}", request_id.0)
 }
