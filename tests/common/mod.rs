4adc21bcc1a68c0098c34e324a715568f9b81c9a
aquatictest <aquatictest@proton.me>
2026-08-21 12:56:52 +0900

    test: add integration tests for post endpoints and update test common helpers

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,8 +1,12 @@
 use std::sync::Arc;
 
-use axum::Router;
+use axum::{
+    Router,
+    body::Body,
+    http::{Method, Request, StatusCode, header},
+};
 use rustboard_api::{
-    configuration::get_configuration,
+    configuration::{Settings, get_configuration},
     repository::{
         comment::PostgresCommentRepository, post::PostgresPostRepository,
         user::PostgresUserRepository,
@@ -10,10 +14,40 @@ use rustboard_api::{
     service::{comment::CommentService, post::PostService, user::UserService},
     state::AppState,
 };
+use serde_json::{Value, json};
 use sqlx::{QueryBuilder, postgres::PgPoolOptions};
+use tower::ServiceExt;
 use uuid::Uuid;
 
 pub async fn test_app() -> Router {
+    let configuration = create_test_db().await;
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&configuration.database.database_url())
+        .await
+        .expect("Failed to connect database");
+
+    let posts_repo = Arc::new(PostgresPostRepository::new(pool.clone()));
+    let comments_repo = Arc::new(PostgresCommentRepository::new(pool.clone()));
+    let users_repo = Arc::new(PostgresUserRepository::new(pool.clone()));
+
+    let post_service = Arc::new(PostService::new(posts_repo.clone()));
+    let comment_service = Arc::new(CommentService::new(posts_repo, comments_repo));
+    let user_service = Arc::new(UserService::new(users_repo));
+
+    let state = AppState {
+        post_service,
+        comment_service,
+        configuration: Arc::new(configuration),
+        pool,
+        user_service,
+    };
+
+    rustboard_api::router::create_router(state)
+}
+
+async fn create_test_db() -> Settings {
     let mut configuration = get_configuration().unwrap();
     configuration.database.database_name = Uuid::new_v4().to_string();
     let pool = PgPoolOptions::new()
@@ -41,25 +75,53 @@ pub async fn test_app() -> Router {
         .expect("Failed to connect database");
 
     sqlx::migrate!("./migrations")
-        .run(&pool)
+        .run(&db_pool)
         .await
         .expect("DB 마이그레이션 실패");
 
-    let posts_repo = Arc::new(PostgresPostRepository::new(db_pool.clone()));
-    let comments_repo = Arc::new(PostgresCommentRepository::new(db_pool.clone()));
-    let users_repo = Arc::new(PostgresUserRepository::new(db_pool.clone()));
+    configuration
+}
 
-    let post_service = Arc::new(PostService::new(posts_repo.clone()));
-    let comment_service = Arc::new(CommentService::new(posts_repo, comments_repo));
-    let user_service = Arc::new(UserService::new(users_repo));
+pub async fn signup_and_login(app_fn: impl Fn() -> Router) -> String {
+    // 회원가입
+    let signup_req = Request::builder()
+        .uri("/signup")
+        .method(Method::POST)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(Body::from(
+            serde_json::to_string(&json!({
+                "email": "test@example.com",
+                "password": "password123",
+                "display_name": "Tester",
+            }))
+            .unwrap(),
+        ))
+        .unwrap();
 
-    let state = AppState {
-        post_service,
-        comment_service,
-        configuration: Arc::new(configuration),
-        pool: db_pool,
-        user_service,
-    };
+    let response = app_fn().oneshot(signup_req).await.unwrap();
+    assert_eq!(response.status(), StatusCode::CREATED);
 
-    rustboard_api::router::create_router(state)
+    // 로그인
+    let login_req = Request::builder()
+        .uri("/login")
+        .method(Method::POST)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(Body::from(
+            serde_json::to_string(&json!({
+                "email": "test@example.com",
+                "password": "password123",
+            }))
+            .unwrap(),
+        ))
+        .unwrap();
+
+    let response = app_fn().oneshot(login_req).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+        .await
+        .unwrap();
+    let response = serde_json::from_slice::<Value>(&body).unwrap();
+
+    response["access_token"].as_str().unwrap().to_string()
 }
